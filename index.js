fe617e041184190c40f454587db72f7a6b48b84d
Arty Buldauskas <abuldauskas@wayfair.com>
2017-03-22 21:11:07 -0400

    first pass at typedef of wasm types to JS

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 export let logger = require('./lib/simple-logger');
 export let heap = require('./lib/simple-heap');
 export let stack = require('./lib/simple-stack');
-export default { logger, heap, stack };
+export const types = require('./lib/types');
+
+export default { logger, heap, stack, types };
 
